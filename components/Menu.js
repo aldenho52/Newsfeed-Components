f5d01a954e3270dfbacc9c07d891de43abfcd6e4
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-09-30 12:59:05 -0700

    completed menu section

--- components/Menu.js
+++ components/Menu.js
@@ -31,3 +31,27 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+function menuMaker (array) {
+  const menu = document.createElement('div')
+  const list = document.createElement('ul')
+
+  menu.appendChild(list)
+  menu.classList.add('menu')
+
+  array.forEach (item => {
+    const listItem = document.createElement('li')
+    listItem.textContent = item
+    list.appendChild(listItem)
+  })
+
+  const menuButton = document.querySelector('.menu-button')
+  menuButton.addEventListener('click', event => {
+    menu.classList.toggle('menu--open')
+  })
+
+  return menu
+}
+
+const header = document.querySelector('.header')
+header.appendChild(menuMaker(menuItems))
